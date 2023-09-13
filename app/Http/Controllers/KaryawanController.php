cde5c456d5c23a42395d1ee82c02083449832eb9
Adreano <9fadreano22@gmail.com>
2023-09-13 11:05:43 +0700

    search pagination

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\karyawan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KaryawanController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        
 
-        $karyawan = DB::table('karyawan')->orderBy('nik')
-        ->join('departemen', 'karyawan.kode_dpt', '=', 'departemen.kode_dpt')
-        ->get();
+        //query search
+        $query = karyawan::query();
+        $query->select('karyawan.*', 'nama_dpt');
+        $query->join('departemen', 'karyawan.kode_dpt', '=', 'departemen.kode_dpt');
+        $query->orderBy('nik');
+        if(!empty($request->nama_karyawan)){
+            $query->where('nama_lengkap','like','%'.$request->nama_karyawan.'%');
+        }
+        if(!empty($request->kode_dpt)){
+            $query->where('karyawan.kode_dpt', $request->kode_dpt);
+        }
+        $karyawan = $query->paginate(2);
 
-        return view('karyawan.index', compact('karyawan'));
+        //menmapilkan data departement di select option
+        $departemen = DB::table('departemen')->get();
+
+        return view('karyawan.index', compact('karyawan','departemen'));
     }
 }
--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -16,7 +16,45 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                    <table class="table table-bordered">
+                    <div class="row">
+                        <div class="col-12">
+                            <form action="/karyawan" method="GET">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <input type="text" name="nama_karyawan" id="nama_karyawan" value="{{ Request('nama_karyawan') }}" class="form-control" placeholder="Nama Karyawan">
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <select name="kode_dpt" id="kode_dpt" class="form-select">
+                                                <option value="">Departemen</option>
+                                                @foreach($departemen as $d)
+                                                    <option {{ Request('kode_dpt')==$d->kode_dpt ? 'selected' : ''}} value="{{ $d->kode_dpt }}">{{ $d->nama_dpt }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary">
+                                            
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                                            <path d="M21 21l-6 -6"></path>
+                                            </svg>
+                                            Cari
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-12">
+                        <table class="table table-bordered">
                     <thead>
                         <tr>
                             <th>NO</th>
@@ -35,7 +73,7 @@
                             $path = Storage::url('uploads/karyawan/'.$d->foto);
                         @endphp
                             <tr>
-                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $loop->iteration + $karyawan->firstItem() - 1 }}</td>
                                 <td>{{ $d->nik }}</td>
                                 <td>{{ $d->nama_lengkap }}</td>
                                 <td>{{ $d->jabatan }}</td>
@@ -53,6 +91,11 @@
                         @endforeach
                     </tbody>
                 </table>
+                <div class="d-flex">
+                {!! $karyawan->links() !!}
+                </div>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
